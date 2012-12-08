2ee78e409925a60902413a3160099cfb1f555f54
Cameron Currie <me@cameroncurrie.net>
2012-12-08 03:04:46 -0600

    Implement account selection

---
+++ DroidBooru/src/in/uncod/android/droidbooru/DroidBooruAccountActivity.java
@@ -0,0 +1,72 @@
+package in.uncod.android.droidbooru;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.res.Resources;
+import android.preference.PreferenceManager;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.google.android.gms.common.AccountPicker;
+
+public class DroidBooruAccountActivity extends SherlockActivity {
+    public static final int REQ_CODE_CHOOSE_ACCOUNT = 8675309;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQ_CODE_CHOOSE_ACCOUNT) {
+            if (data != null) {
+                storeAccount(data);
+            }
+            else {
+                // No account selected
+                finish();
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public Account getDroidBooruAccount() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Resources resources = getResources();
+        Account retAccount = null;
+
+        // Get stored account if we have one
+        String storedAccountName = prefs.getString(resources.getString(R.string.pref_account_name), "");
+        if (storedAccountName != "") {
+            retAccount = new Account(storedAccountName, prefs.getString(
+                    resources.getString(R.string.pref_account_type), ""));
+        }
+        else {
+            // Let user pick account
+            Intent intent = AccountPicker.newChooseAccountIntent(null, null, new String[] { "com.google" },
+                    false, null, null, null, null);
+            startActivityForResult(intent, REQ_CODE_CHOOSE_ACCOUNT);
+        }
+
+        return retAccount;
+    }
+
+    private void storeAccount(Intent data) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Resources resources = this.getResources();
+
+        String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+        String accountType = data.getStringExtra(AccountManager.KEY_ACCOUNT_TYPE);
+
+        storeAccountInPrefs(new Account(accountName, accountType), prefs, resources);
+    }
+
+    private void storeAccountInPrefs(Account account, SharedPreferences prefs, Resources resources) {
+        Editor editor = prefs.edit();
+
+        editor.putString(resources.getString(R.string.pref_account_name), account.name);
+        editor.putString(resources.getString(R.string.pref_account_type), account.type);
+
+        editor.commit();
+    }
+}
--- DroidBooru/src/in/uncod/android/droidbooru/MainActivity.java
+++ DroidBooru/src/in/uncod/android/droidbooru/MainActivity.java
@@ -13,24 +13,17 @@ import java.util.List;
 import org.apache.http.HttpHost;
 
 import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.MenuItem;
 
-import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
 
-public class MainActivity extends SherlockActivity {
+public class MainActivity extends DroidBooruAccountActivity {
     private static final String TAG = "MainActivity";
 
     /**
@@ -38,9 +31,9 @@ public class MainActivity extends SherlockActivity {
      */
     private static final int REQ_CODE_GET_FILE = 0;
 
-    private Account mAccount;
-
     private Handler mUiHandler;
+
+    private Account mAccount;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -71,80 +64,87 @@ public class MainActivity extends SherlockActivity {
 
         mUiHandler = new Handler();
 
-        mAccount = getDroidBooruAccount(this);
-
         // Start notification service
         Intent service = new Intent(this, NotificationService.class);
         startService(service);
+    }
 
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEND.equals(intent.getAction())) {
-            // Started via Share request
-            Log.d(TAG, "Received single file upload request");
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-            Uri shareUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-            if (shareUri == null) {
-                // No direct file URI; see if there's a URI in the text extra
-                shareUri = Uri.parse(intent.getStringExtra(Intent.EXTRA_TEXT));
-            }
+        mAccount = getDroidBooruAccount();
 
-            Log.d(TAG, "Shared URI is " + shareUri);
+        if (mAccount != null) {
+            Intent intent = getIntent();
+            if (Intent.ACTION_SEND.equals(intent.getAction())) {
+                // Started via Share request
+                Log.d(TAG, "Received single file upload request");
 
-            if (shareUri.getScheme().equals(HttpHost.DEFAULT_SCHEME_NAME)) {
-                // URI is HTTP link to file; download it first
-                try {
-                    Backend.getInstance().downloadTempFileFromHttp(shareUri,
-                            new OnTaskResultListener<List<File>>() {
-                                public void onTaskResult(List<File> result) {
-                                    if (result.size() > 0) {
-                                        uploadSingleFile(Uri.fromFile(result.get(0)));
-                                    }
-                                    else {
-                                        setResult(RESULT_CANCELED);
-                                        finish();
-                                    }
-                                }
-                            }, GalleryActivity.createDownloadingProgressDialog(this));
+                Uri shareUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                if (shareUri == null) {
+                    // No direct file URI; see if there's a URI in the text extra
+                    shareUri = Uri.parse(intent.getStringExtra(Intent.EXTRA_TEXT));
                 }
-                catch (MalformedURLException e) {
-                    e.printStackTrace();
 
-                    setResult(RESULT_CANCELED);
+                Log.d(TAG, "Shared URI is " + shareUri);
+
+                if (shareUri.getScheme().equals(HttpHost.DEFAULT_SCHEME_NAME)) {
+                    // URI is HTTP link to file; download it first
+                    try {
+                        Backend.getInstance().downloadTempFileFromHttp(shareUri,
+                                new OnTaskResultListener<List<File>>() {
+                                    public void onTaskResult(List<File> result) {
+                                        if (result.size() > 0) {
+                                            uploadSingleFile(Uri.fromFile(result.get(0)));
+                                        }
+                                        else {
+                                            setResult(RESULT_CANCELED);
+                                            finish();
+                                        }
+                                    }
+                                }, GalleryActivity.createDownloadingProgressDialog(this));
+                    }
+                    catch (MalformedURLException e) {
+                        e.printStackTrace();
+
+                        setResult(RESULT_CANCELED);
+                        finish();
+                    }
+                }
+                else {
+                    // Uploading a single file
+                    uploadSingleFile(shareUri);
+                }
+            }
+            else if (Intent.ACTION_SEND_MULTIPLE.equals(intent.getAction())) {
+                // Started via Share request
+                Log.d(TAG, "Received multiple file upload request");
+
+                // Uploading multiple files
+                ArrayList<Uri> fileUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+                File[] files = new File[fileUris.size()];
+
+                int i = 0;
+                for (Uri uri : fileUris) {
+                    files[i] = Backend.getInstance().createTempFileForUri(uri, getContentResolver());
+
+                    i++;
+                }
+
+                uploadMultipleFiles(files);
+            }
+            else {
+                // Need to launch the gallery
+                Intent galleryIntent = new Intent(this, GalleryActivity.class).setAction(intent.getAction());
+
+                if (Intent.ACTION_GET_CONTENT.equals(intent.getAction())) {
+                    startActivityForResult(galleryIntent, REQ_CODE_GET_FILE);
+                }
+                else {
+                    startActivity(galleryIntent);
                     finish();
                 }
-            }
-            else {
-                // Uploading a single file
-                uploadSingleFile(shareUri);
-            }
-        }
-        else if (Intent.ACTION_SEND_MULTIPLE.equals(intent.getAction())) {
-            // Started via Share request
-            Log.d(TAG, "Received multiple file upload request");
-
-            // Uploading multiple files
-            ArrayList<Uri> fileUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
-            File[] files = new File[fileUris.size()];
-
-            int i = 0;
-            for (Uri uri : fileUris) {
-                files[i] = Backend.getInstance().createTempFileForUri(uri, getContentResolver());
-
-                i++;
-            }
-
-            uploadMultipleFiles(files);
-        }
-        else {
-            // Need to launch the gallery
-            Intent galleryIntent = new Intent(this, GalleryActivity.class).setAction(intent.getAction());
-
-            if (Intent.ACTION_GET_CONTENT.equals(intent.getAction())) {
-                startActivityForResult(galleryIntent, REQ_CODE_GET_FILE);
-            }
-            else {
-                startActivity(galleryIntent);
-                finish();
             }
         }
     }
@@ -183,42 +183,6 @@ public class MainActivity extends SherlockActivity {
                 }, GalleryActivity.createUploadingProgressDialog(MainActivity.this));
     }
 
-    public static Account getDroidBooruAccount(Context context) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        Resources resources = context.getResources();
-        Account retAccount = null;
-
-        // Get stored account if we have one
-        String storedAccountName = prefs.getString(resources.getString(R.string.pref_account_name), "");
-        if (storedAccountName != "") {
-            retAccount = new Account(storedAccountName, prefs.getString(
-                    resources.getString(R.string.pref_account_type), ""));
-        }
-        else {
-            Account[] accounts = AccountManager.get(context).getAccounts();
-            if (accounts.length > 0) {
-                // TODO Let user pick account
-                for (Account account : accounts) {
-                    if (account.name.endsWith("ironclad.mobi")) {
-                        retAccount = account;
-                        break;
-                    }
-                }
-
-                if (retAccount == null)
-                    retAccount = createDefaultAccount();
-
-                storeAccountInPrefs(retAccount, prefs, resources);
-            }
-            else {
-                // Proceed with a default account
-                retAccount = createDefaultAccount();
-            }
-        }
-
-        return retAccount;
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQ_CODE_GET_FILE) {
@@ -229,18 +193,5 @@ public class MainActivity extends SherlockActivity {
         else {
             super.onActivityResult(requestCode, resultCode, data);
         }
-    }
-
-    private static void storeAccountInPrefs(Account account, SharedPreferences prefs, Resources resources) {
-        Editor editor = prefs.edit();
-
-        editor.putString(resources.getString(R.string.pref_account_name), account.name);
-        editor.putString(resources.getString(R.string.pref_account_type), account.type);
-
-        editor.commit();
-    }
-
-    private static Account createDefaultAccount() {
-        return new Account("droidbooru@ironclad.mobi", "Google");
     }
 }
